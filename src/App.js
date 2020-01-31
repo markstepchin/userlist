602fdbeea6eba0c4c72285c249c6513ca4645f07
Mark Stepchin <mstepchin@urbanova.org>
2020-01-30 23:03:45 -0800

    fetch user data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Users from "./Users";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      await fetch("https://jsonplaceholder.typicode.com/users")
+        .then(res => res.json())
+        .then(json => setData(json))
+        .catch(error => console.log("error: ", error));
+    }
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box mt={5}>
+      <Grid container justify="space-around" alignItems="center">
+        <Grid item>
+          <Users data={data} />
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
 
